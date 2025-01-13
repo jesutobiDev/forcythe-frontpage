06a0ba971c3143a8ee8befa0ce28badc0abffb64
Bada Jesutobi <jesutobi_dev@outlook.com>
2025-01-13 15:16:34 +0100

    nav toggle functionality added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState} from "react";
+import { useState, useRef, useEffect } from "react";
 import logo from "../assets/forcythe logo.svg";
 import { NavLink, Link } from "react-router-dom";
 import Button from "./Button";
@@ -6,12 +6,28 @@ import { HiOutlineMenuAlt2 } from "react-icons/hi";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
     setIsMenuOpen((prevState) => !prevState);
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setIsMenuOpen(false);
+    }
+  };
 
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isMenuOpen]);
 
   return (
     <nav className="fixed top-0 left-0 w-full h-24 bg-transparent backdrop-blur-lg flex md:gap-5 lg:gap-20 items-center px-5 md:px-10 lg:px-12 xl:px-32 justify-between shadow-md z-50">
@@ -27,6 +43,7 @@ const Navbar = () => {
       </button>
       {/* Navigation Links */}
       <div
+        ref={menuRef}
         className={`${
           isMenuOpen ? "flex" : "hidden"
         } absolute top-28 bg-primary rounded-3xl md:rounded-none md:bg-transparent w-1/2 md:w-auto h-fit right-5 md:static flex-col md:flex-row md:items-center p-7 md:p-0 gap-7 md:gap-5 lg:mr-auto md:flex`}
@@ -56,7 +73,7 @@ const Navbar = () => {
       {/* Call-to-Action Button */}
       <div className="hidden md:flex">
         <Button>Book a Call</Button>
-      </div>{" "}
+      </div>
     </nav>
   );
 };
